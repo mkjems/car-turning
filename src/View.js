a43f8bfbc48369d600ab2eaecc646ae04d5a4223
Martin Kjems <mkjems@gmail.com>
2026-05-31 19:48:34 +0200

    removed stats

--- src/View.js
+++ src/View.js
@@ -3,10 +3,8 @@ var Ray =require('./Ray.js').Ray;
 
 // Setup
 var canvasElm = document.getElementById("game_canvas");
-var statsCanvasElm = document.getElementById("stats_canvas");
 
 var ctx = canvasElm.getContext("2d");
-var stats_ctx = statsCanvasElm.getContext("2d");
 
 exports.cars = [new Car(), new Car()];
 
@@ -26,20 +24,8 @@ function drawCars(state) {
 	// ray2.draw(ctx, 0, canvasElm.width);
 }
 
-function drawStats(state) {
-	// shift everything to the left
-	var imageData = stats_ctx.getImageData(1, 0, statsCanvasElm.width-1, statsCanvasElm.height);
-	stats_ctx.putImageData(imageData, 0, 0);
-	// Clear line along right edge
-	stats_ctx.clearRect(stats_ctx.canvas.width-1, 0, 1, stats_ctx.canvas.height);
-	// Draw line along the right edge
-	stats_ctx.fillStyle = "#000";
-	stats_ctx.fillRect(99, (99-state.stats.time_usage_percentage), 1, 1);
-}
 
 // Render everything
 exports.renderFrame = function(state) {
-	//clearFrame();
-	//drawStats(state);
 	drawCars(state);
 };
